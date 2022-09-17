2c04063db9e1dcf02c24310e3505b251a050ccf3
Admin <hoangnd2@paditech.com>
2022-09-17 17:12:21 +0700

    3

--- src/app/controller/SiteControllers.js
+++ src/app/controller/SiteControllers.js
@@ -19,16 +19,77 @@ class SiteControllers {
        
 
     // }
+
+    //GET /
     index(req,res,next){
-        db.User.findAll()
+        db.User.findAll({raw: true})  // thêm raw: true để sử dụng dữ liệu như mongoose
         .then(data =>{
             
             res.render('home',{
-                data: JSON.stringify(data)
+                data: data
             })
 
         })
         .catch(next)
+    }
+    create(req,res){
+        res.render('data/create')
+    }
+    
+    //POST /data/store
+    store(req,res){       // sử lý thêm thông tin người dùng vào cơ sở dữ liệu (khác so với mongoDB - xem ở folder nodejs)
+       const data = req.body
+       db.User.create({
+        name: data.name,
+        email: data.email,
+        age: data.age,
+        gender: data.gender
+
+       })
+       .then(() => res.redirect('/'))  // sau khi thêm khóa học xog trở về trang chủ
+        .catch(error => {
+        })
+        
+    }
+
+    //GET /date/:id/edit
+    edit(req,res,next){
+        db.User.findByPk(req.params.id,{raw: true})
+        .then(data => res.render('data/edit' ,{
+            data: data
+        }))
+        .catch(next)
+    }
+    
+    //PUT /data/:id
+
+    update(req,res,next){
+        const data = req.body
+        db.User.update({
+            name: data.name,
+            email: data.email,
+            age: data.age,
+            gender: data.gender
+        
+        },
+
+        {where: {id: req.params.id}}
+        
+        
+        
+        ) // cập nhật lại , {_id: req.params.id} là phần điều kiện
+        .then(() => res.redirect('/'))  // sau khi thành công trở về trang mình mong muốn
+        .catch(next)
+        
+    }
+    
+    destroy(req,res,next){
+        db.User.destroy({
+            where: {id: req.params.id}
+        })
+        .then(() => res.redirect('/'))  // sau khi thành công trở về trang mình mong muốn
+        .catch(next)
+        
     }
 
 
--- src/routes/site.js
+++ src/routes/site.js
@@ -4,7 +4,11 @@ const router = express.Router();
 
 const siteControllers = require('../app/controller/SiteControllers');
 
-
+router.get('/data/create', siteControllers.create);
+router.post('/data/store', siteControllers.store);   // router.get/post/put : đặt tên theo đúng method
+router.get('/data/:id/edit', siteControllers.edit);
+router.put('/data/:id', siteControllers.update);
+router.delete('/data/delete/:id', siteControllers.destroy);
 router.get('/', siteControllers.index);
 
 module.exports = router;
